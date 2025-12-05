39d2b7ec61217819682b5786122fd49f00fc59a8
Samuel Bosawer <zam.bosawer@gmail.com>
2025-12-05 12:34:40 +0900

    Perbaiki route

--- routes/admin/mahasiswa.php
+++ routes/admin/mahasiswa.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => ['auth']], function () {
 
     Route::controller(Mahasiswa::class)->group(function(){
-        Route::get('mahasiswa', [Mahasiswa::class, 'index'])->name('mahasiswa')->middleware(['auth','role.custom:mahasiswa']);
+        Route::get('mahasiswa', [Mahasiswa::class, 'index'])->name('mahasiswa')->middleware(['auth','role.custom:mahasiswa|adminprodi']);
         Route::get('mahasiswa/tambah', [Mahasiswa::class, 'create'])->name('mahasiswa.tambah')->middleware(['auth','role.custom:adminprodi']);
-        Route::get('mahasiswa/detail/{id}', [Mahasiswa::class, 'show'])->name('mahasiswa.detail')->middleware(['auth','role.custom:mahasiswa']);
+        Route::get('mahasiswa/detail/{id}', [Mahasiswa::class, 'show'])->name('mahasiswa.detail')->middleware(['auth','role.custom:mahasiswa}adminprodi']);
         Route::delete('mahasiswa/{id}', [Mahasiswa::class, 'destroy'])->name('mahasiswa.hapus')->middleware(['auth','role.custom:adminprodi']);
         Route::post('mahasiswa/store', [Mahasiswa::class, 'store'])->name('mahasiswa.store')->middleware(['auth','role.custom:adminprodi']);
         Route::get('mahasiswa/{id}/ubah', [Mahasiswa::class, 'edit'])->name('mahasiswa.ubah')->middleware(['auth','role.custom:adminprodi']);
